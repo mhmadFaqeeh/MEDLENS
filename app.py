a05936597c3587a2d3823819a818702b6a8094d3
mhmadFaqeeh <160781715+mhmadFaqeeh@users.noreply.github.com>
2026-04-08 06:15:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,5 @@ def get_db_connection():
         database=os.environ.get("MYSQLDATABASE"),
         port=int(os.environ.get("MYSQLPORT", 3306))
     )
+    if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
