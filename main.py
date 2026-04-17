a0443472dc21649b8f92c77caf7d8978da7f5963
mhmadFaqeeh <160781715+mhmadFaqeeh@users.noreply.github.com>
2026-04-17 11:48:28 -0700

    Add test route that returns a success message

--- main.py
+++ main.py
@@ -78,3 +78,7 @@ def scan_drug():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+    @app.route('/test')
+def test():
+    return "السيرفر شغال تمام يا محمد!"
+    
